e7b3fab13b9024867a66032c5beb00dc0b73daca
snofamv <74159423+snofamv@users.noreply.github.com>
2025-01-23 09:45:46 -0300

    implementacion del datatable

--- src/persons/pages/PersonsPage.tsx
+++ src/persons/pages/PersonsPage.tsx
@@ -2,23 +2,25 @@ import { Grid, Typography } from "@mui/material";
 import DataTable from "../components/table/DataTable";
 import { Loader } from "../components/ui/Loader";
 import { usePersons } from "../hooks";
+import { useNavigate } from "react-router";
+import { deletePersonById } from "../helpers/deletePersonById";
 // import { useNavigate } from "react-router";
 // import { deletePersonById } from "../helpers/deletePersonById";
 
 export const PersonsPage = () => {
-  const { loading, persons, error } = usePersons();
-  // const navigate = useNavigate();
-  // const handleDelete = async (id: string) => {
-  //   try {
-  //     await deletePersonById(id);
-  //     setOnDelete(true);
-  //   } catch (err) {
-  //     console.error("Error al eliminar persona:", err);
-  //   }
-  // };
-  // const handleUpdatePerson = (id: string) => {
-  //   navigate(`/search/${id}`);
-  // };
+  const { loading, persons, error, setOnDelete } = usePersons();
+  const navigate = useNavigate();
+  const handleDeletePerson = async (id: string) => {
+    try {
+      await deletePersonById(id);
+      setOnDelete(true);
+    } catch (err) {
+      console.error("Error al eliminar persona:", err);
+    }
+  };
+  const handleUpdatePerson = (id: string) => {
+    navigate(`/search/${id}`);
+  };
 
   return (
     <Grid
@@ -30,7 +32,11 @@ export const PersonsPage = () => {
       {loading && <Loader />}
       {error && <Typography variant="h2">Ops.. Hubo un error.</Typography>}
       {!loading && !error && persons && persons.length > 0 && (
-        <DataTable dataList={persons} />
+        <DataTable
+          dataList={persons}
+          onDelete={handleDeletePerson}
+          onUpdate={handleUpdatePerson}
+        />
       )}
       {!loading && !error && persons && persons.length === 0 && (
         <Typography variant="h2" style={{ marginTop: 100 }}>
